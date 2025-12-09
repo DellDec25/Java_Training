41d68759e870d3f1b81c9f61e0cd94f61271c765
Sheredell D. Arnaiz <sdarnaiz@headoffice.corp.bpi.com.ph>
2025-12-09 18:26:42 +0800

    Add Activity 2

---
+++ M1_Acticity2.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ph.com.bpi.practice.main;
+
+/**
+ * 
+ */
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		Whiteboard wb = new Whiteboard();
+		int total = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(total));
+	}
+}
